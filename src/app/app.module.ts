b352dba8ebd659396a0c19c0330b1f4b3f29262b
mohamed <moham@Laptop>
2021-02-25 14:13:03 +0200

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MainNavComponent } from './layout/main-nav/main-nav.component';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
